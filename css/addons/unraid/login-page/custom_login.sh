842158492b3015fb363e29135aed84edb359a187
winthed <68131874+winthed@users.noreply.github.com>
2022-06-09 23:00:21 +0200

    Update custom_login.sh

--- css/addons/unraid/login-page/custom_login.sh
+++ css/addons/unraid/login-page/custom_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TYPE="retro-terminal"
-THEME="green.css"
-DOMAIN="theme-park.dev" # If you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
+THEME="blue.css"
+DOMAIN="github.com/winthed" # If you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
 ADD_JS="true"
 JS="custom_text_header.js"
 DISABLE_THEME="false"
